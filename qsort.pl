c10254c46a15216775c924514a2e0ad9413351fe
Antón Maestre <161016788+AntonMG4@users.noreply.github.com>
2024-03-19 09:07:18 +0100

    Update qsort.pl

--- qsort.pl
+++ qsort.pl
@@ -5,13 +5,18 @@
 	los elementos de Lista ordenados de menor a mayor
 
 	divide(+Pivote, +Lista, -Menores, -Mayores)
+	es cierto si Menores unifica con una lista que contiene
+	los elementos de Lista menores que el Pivote y Mayores
+	los elementos mayores que el Pivote
 	
 */
 
 divide(_, [], [], []).
 
-divide(Elem, [Cab|Resto], [Cab|R1], R2) :- Cab=<Elem, divide(Elem, Resto, R1, R2).
-divide(Elem, [Cab|Resto], R1, [Cab|R2]) :- Cab>Elem, divide(Elem, Resto, R1, R2).
+divide(Elem, [Cab|Resto], [Cab|R1], R2) :- Cab =< Elem, divide(Elem, Resto, R1, R2).
+divide(Elem, [Cab|Resto], R1, [Cab|R2]) :- Cab > Elem, divide(Elem, Resto, R1, R2).
 
 qsort([], []).
-qsort([Cab|Resto], ) :- divide(Cab, Resto, Men, May), qsort(Resto, R).
+qsort([Elem],[Elem]).
+qsort([Cab|Resto], R2) :- divide(Cab, Resto, Men, May), append(Menores, [Cab|Mayores], R2), 
+qsort(Men, Menores), qsort(May, Mayores).
